d804465ca366b2df1fc73b03cf9a129a24d3208b
Lars Tørnes Hansen <devel@larsth.dk>
2015-11-25 22:23:04 +0100

    Type Payload: hide struct fields: MessageOctets, HMACOctets, and update mkcoverage.bash output files  Modified: 	modified:   benchmarks.txt 	modified:   coverage.html 	modified:   payload.go

--- payload.go
+++ payload.go
@@ -27,8 +27,8 @@ type Payload struct {
 	mutex         sync.Mutex
 	Secrets       Secrets
 	Message       Message
-	MessageOctets []byte
-	HMACOctets    []byte
+	messageOctets []byte
+	hMACOctets    []byte
 }
 
 func createHMAC(hmacKey, salt, messageOctets []byte) (octets []byte, err error) {
@@ -122,7 +122,7 @@ func (p *Payload) MarshalBinary() (data []byte, err error) {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
-	p.MessageOctets = nil
+	p.messageOctets = nil
 	data = nil
 	if len(p.Secrets.HMACKey) == 0 {
 		//HMAC secret is not set ...
@@ -134,12 +134,12 @@ func (p *Payload) MarshalBinary() (data []byte, err error) {
 		err = ErrNiSaltSlice
 		return
 	}
-	p.MessageOctets = make([]byte, messageOctets, PayloadOctets)
+	p.messageOctets = make([]byte, messageOctets, PayloadOctets)
 
 	//the message ...
 	//	1.0 Marshal The Gps structure to binary ...
 	//	1.1 Marshal the FixMode value
-	p.MessageOctets[0], err = p.Message.Gps.FixMode.marshalByte()
+	p.messageOctets[0], err = p.Message.Gps.FixMode.marshalByte()
 	if err != nil {
 		return nil, err
 	}
@@ -148,52 +148,52 @@ func (p *Payload) MarshalBinary() (data []byte, err error) {
 	// 1.2 Marshal the Latitude IEEE 754 32-bit floating-point value
 	v1, v2, v3, v4 = float32MarshalBinaryValues(p.Message.Gps.Latitude)
 	//Example latitude: float32(55.69147) -> v1=0x42, v2=0x5e, v3=0xc4, v4=0x11
-	p.MessageOctets[1] = v1
-	p.MessageOctets[2] = v2
-	p.MessageOctets[3] = v3
-	p.MessageOctets[4] = v4
+	p.messageOctets[1] = v1
+	p.messageOctets[2] = v2
+	p.messageOctets[3] = v3
+	p.messageOctets[4] = v4
 	// 1.3 Marshal the Longitude IEEE 754 32-bit floating-point value
 	v1, v2, v3, v4 = float32MarshalBinaryValues(p.Message.Gps.Longitude)
 	//Example longitude: float32(12.61681) -> v1 = 0x41, v2=0x49, v3=0xde, v4=0x74
-	p.MessageOctets[5] = v1
-	p.MessageOctets[6] = v2
-	p.MessageOctets[7] = v3
-	p.MessageOctets[8] = v4
+	p.messageOctets[5] = v1
+	p.messageOctets[6] = v2
+	p.messageOctets[7] = v3
+	p.messageOctets[8] = v4
 	// 1.4 Marshal the Altitude IEEE 754 32-bit floating-point value
 	v1, v2, v3, v4 = float32MarshalBinaryValues(p.Message.Gps.Altitude)
 	//Example altitude: float32(2.01) -> v1=0x40, v2=0x00, v3=0xa3, v4=0xd7
-	p.MessageOctets[9] = v1
-	p.MessageOctets[10] = v2
-	p.MessageOctets[11] = v3
-	p.MessageOctets[12] = v4
+	p.messageOctets[9] = v1
+	p.messageOctets[10] = v2
+	p.messageOctets[11] = v3
+	p.messageOctets[12] = v4
 	// 2.0 Marshal the TimeStamp
 	v1, v2, v3, v4, v5, v6, v7, v8 = p.Message.TimeStamp.marshalBytes()
 	//Example timestamp: "2015-11-21T08:41:55Z"
 	//	and reference time is "2305-01-01T00:00:00Z" ->
 	// 		v1=0x81, v2=0x62, v3=0xf2, v4=0xa9,
 	// 		v5=0x91, v6=0x2f, v7=0x7e, v8=0x00
-	p.MessageOctets[13] = v1
-	p.MessageOctets[14] = v2
-	p.MessageOctets[15] = v3
-	p.MessageOctets[16] = v4
-	p.MessageOctets[17] = v5
-	p.MessageOctets[18] = v6
-	p.MessageOctets[19] = v7
-	p.MessageOctets[20] = v8
+	p.messageOctets[13] = v1
+	p.messageOctets[14] = v2
+	p.messageOctets[15] = v3
+	p.messageOctets[16] = v4
+	p.messageOctets[17] = v5
+	p.messageOctets[18] = v6
+	p.messageOctets[19] = v7
+	p.messageOctets[20] = v8
 
 	//hashsum ...
 	// 3.0 Create HMAC: 224-bits(=28 bytes) using the
 	// SHA2-256-224 hashsum algorithm ...
 	//The error from createHMAC is ignored, because the code in this
 	//method will make sure the error is always nil ...
-	p.HMACOctets, _ = createHMAC(p.Secrets.HMACKey, p.Secrets.Salt,
-		p.MessageOctets)
+	p.hMACOctets, _ = createHMAC(p.Secrets.HMACKey, p.Secrets.Salt,
+		p.messageOctets)
 
 	// 3.1 create the 'data' byte slice:
-	capacity := len(p.MessageOctets) + len(p.HMACOctets)
+	capacity := len(p.messageOctets) + len(p.hMACOctets)
 	data = make([]byte, 0, capacity)
-	data = append(data, p.MessageOctets...)
-	data = append(data, p.HMACOctets...)
+	data = append(data, p.messageOctets...)
+	data = append(data, p.hMACOctets...)
 
 	// 4.0 Done!
 	return data, nil
@@ -220,12 +220,12 @@ func (p *Payload) UnmarshalBinary(data []byte) error {
 	}
 
 	// 2.0 Split the 'data' slice into the message slice and the hmac slice ...
-	p.MessageOctets = data[:messageOctets]
-	p.HMACOctets = data[messageOctets:]
+	p.messageOctets = data[:messageOctets]
+	p.hMACOctets = data[messageOctets:]
 
 	// 	3.0 HMAC checking ...
-	err = checkHMAC(p.Secrets.HMACKey, p.Secrets.Salt, p.MessageOctets,
-		p.HMACOctets)
+	err = checkHMAC(p.Secrets.HMACKey, p.Secrets.Salt, p.messageOctets,
+		p.hMACOctets)
 	if err != nil {
 		return err
 	}
@@ -234,26 +234,26 @@ func (p *Payload) UnmarshalBinary(data []byte) error {
 
 	// 4.1.0 Unmarshal the GPS POI ...
 	// 4.1.1 Unmarshal the FixMode ...
-	b = p.MessageOctets[0]
+	b = p.messageOctets[0]
 	if err = (&p.Message.Gps.FixMode).unmarshalByte(b); err != nil {
 		return err
 	}
 	// 4.1.2 Unmarshal the latitude ...
-	s = p.MessageOctets[1:5]
+	s = p.messageOctets[1:5]
 	f = float32UnmarshalBinary(s)
 
 	p.Message.Gps.Latitude = f
 	// 4.1.2 Unmarshal the longitude ...
-	s = p.MessageOctets[5:9]
+	s = p.messageOctets[5:9]
 	f = float32UnmarshalBinary(s)
 	p.Message.Gps.Longitude = f
 	// 4.1.2 Unmarshal the altitude ...
-	s = p.MessageOctets[9:13]
+	s = p.messageOctets[9:13]
 	f = float32UnmarshalBinary(s)
 	p.Message.Gps.Altitude = f
 
 	// 4.2 Unmarshal the timestamp
-	s = p.MessageOctets[13:]
+	s = p.messageOctets[13:]
 	p.Message.TimeStamp.unmarshalBytes(
 		s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7])
 
